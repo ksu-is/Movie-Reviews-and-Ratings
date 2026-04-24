60446a23dbfff359ca897c00aae4b995269a5182
Gpaw123 <gpaw@students.kennesaw.edu>
2026-04-24 18:43:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 users = []
-movies = []
+movies = [
+    {
+        "Title": "...",
+        "Year": "...",
+        "Synopsis": "...",
+        "Cast": "...",
+        "Reviews": [],
+        "Ratings": []
+    },
+    {
+        "Title": "...",
+        "Year": "...",
+        "Synopsis": "...",
+        "Cast": "...",
+        "Reviews": [],
+        "Ratings": []
+    },
+]
 
 def login():
     print("Welcome to MRR!")
@@ -11,3 +28,22 @@ def login():
         users.append(username)
         print("Welcome", username)
         return username
+    
+def add_review(username, movie):
+    review = input("Enter review: ")
+    if review != "":
+        movie["reviews"].append(username + ": " + review)
+        print("Review added")
+
+def add_rating(movie):
+    rating = input("Enter rating (1-5): ")
+    if rating.isdigit():
+        rating = int(rating)
+        movie["ratings"].append(rating)
+        print("Rating added")
+
+
+       
+
+
+
