b98caaca3e89c6cfa9beb43db0b207e5b7e3d94c
Gpaw123 <gpaw@students.kennesaw.edu>
2026-04-24 19:51:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,61 @@ def login():
         print("Welcome", username)
         return username
     
+def movie_page(username, movie):
+    while True:
+        print(movie["title"])
+        print("Year: ", movie["year"])
+        print("Synopsis: ", movie["synopsis"])
+        print("Main cast:", movie["cast"])
+
+        print("\nReviews:")
+        if len(movie["reviews"]) == 0:
+            print("No reviews made")
+        else:
+            for review in movie["reviews"]:
+                print(review)
+
+        print("\nRatings:")
+        if len(movie["rating"]) == 0:
+            print("No ratings made")
+        else:
+            # calculates average rating
+            rating_avg = sum(movie["ratings"]) / len(movie["ratings"])
+            #shows average rating of movie and number of ratings made
+            print("Average rating: " + str(round(rating_avg, 1)) + "/5 (" + str(len(movie["ratings"])) + " rating made)")
+
+        choice = input("\n1. Add review"
+                            "2. Add rating"
+                            "3. Go back to movie list")
+        
+        if choice == "1":
+            add_review(username, movie)
+
+        elif choice == "2":
+            add_rating(movie)
+
+        elif choice == "3":
+            return
+
+def browse_movies(username):
+    while True:
+        for items in range(len(movies)):
+            #Get position of movies and its title
+            print(items + 1 + ". " + movies[items]["title"])
+        
+        choice = input("\n1. Select movie"
+                       "2. Go back to main menu")
+        
+        if choice == "1":
+            movie_name = input("Enter movie: ")
+            for movie in movies:
+                if movie["title"].lower() == movie_name.lower():
+                    # displays movie info
+                    movie_page(username, movie)
+                    break
+                else: 
+                    print("Movie not found")
+    
 def add_review(username, movie):
     review = input("Enter review: ")
     if review != "":
